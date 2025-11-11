72eb7cb73867ee203bda2ce33c27691fd2436e98
gabatxo1312 <admin1312@kl.netlib.re>
2025-11-11 18:12:33 +0100

    Add operation status to display status alert

--- src/lib.rs
+++ src/lib.rs
@@ -23,7 +23,7 @@ pub fn router(state: state::AppState) -> Router {
         .route("/categories", get(routes::category::index))
         .route("/categories", post(routes::category::create))
         .route("/categories/new", get(routes::category::new))
-        .route("/categories/{id}/delete", post(routes::category::delete))
+        .route("/categories/{id}/delete", get(routes::category::delete))
         // Register static assets routes
         .nest("/assets", static_router())
         // Insert request timing
